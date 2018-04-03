68cf32b2c0259e1f57699f7c2d46865fd74ec55b
sazcure <sazcure@github.com>
2018-04-03 11:15:39 +0530

    ..

--- js/index.js
+++ js/index.js
@@ -5,8 +5,8 @@ $(window).on('scroll', () => {
     let wScroll = $(this).scrollTop();
 
     //keeping the welcome div in center while the parallax scrolling
-    $('#welcome > h3').css({ 'transform': `translateY(${wScroll / 3}px)` });
-    $('#welcome > h1').css({ 'transform': `translateY(${wScroll / 3}px)` });
+    $('#welcome > h3').css({ 'transform': `translateY(${wScroll / 2}px)` });
+    $('#welcome > h1').css({ 'transform': `translateY(${wScroll / 2}px)` });
 
 
     prevScrollPos = wScroll;
